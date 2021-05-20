3bd3c830e4df56fc0efe6d58407d4ae4dc497363
Dilan <dilan.uriostegui@gmail.com>
2021-05-20 14:16:16 -0500

    Creacion del proyecto agregando el navbar y las rutas

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { FotosComponent } from './components/fotos/fotos.component';
+import { CargaComponent } from './components/carga/carga.component';
+import { APP_ROUTES } from './app.routes';
+import { CargaImagenesService } from './services/carga-imagenes.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FotosComponent,
+    CargaComponent
+  ],
+  imports: [
+    BrowserModule,
+    APP_ROUTES
+  ],
+  providers: [
+    CargaImagenesService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
